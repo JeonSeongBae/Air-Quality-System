2995b9341afd11be2a5ec70c1c40538a6876d8c2
leechungwan <cnddhks846@naver.com>
2018-08-10 18:01:56 +0900

    Add two function (makeCoordinate, drawLine)

--- testVersion/app/src/main/java/com/example/leechungwan/testversion/MainActivity.java
+++ testVersion/app/src/main/java/com/example/leechungwan/testversion/MainActivity.java
@@ -4,6 +4,8 @@ import android.graphics.Color;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,21 +23,24 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private List<EndDevice> list_EndDevice;
+    private List<MapCoordinate> coordinates;
+    private LatLng[] latLngList;
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference firebaseDatabaseRef;
     private GoogleMap mGoogleMap = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         list_EndDevice = new ArrayList<EndDevice>();
-
+        coordinates = makeCoordinate();
 
         android.app.FragmentManager fragmentManager = getFragmentManager();
-        MapFragment mapFragment = (MapFragment)fragmentManager
+        MapFragment mapFragment = (MapFragment) fragmentManager
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
@@ -45,6 +50,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         updateNode();
 
     }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
@@ -53,33 +59,72 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(startLocation));
         googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
     }
-    private void initNode(GoogleMap googleMap){
-        LatLng[] latLngList = new LatLng[list_EndDevice.size()];
-        for(int i=0; i<list_EndDevice.size();i++){
+
+    private void initNode(GoogleMap googleMap) {
+        latLngList = new LatLng[list_EndDevice.size()];
+        for (int i = 0; i < list_EndDevice.size(); i++) {
             latLngList[i] = new LatLng(list_EndDevice.get(i).getLatitude(), list_EndDevice.get(i).getLongitude());
         }
 
-        for(int idx = 0; idx<5; idx++){
+        for (int idx = 0; idx < list_EndDevice.size(); idx++) {
             MarkerOptions markerOptions = new MarkerOptions();
             markerOptions
                     .position(latLngList[idx])
-                    .title(list_EndDevice.get(idx).getID());
+                    //.title(list_EndDevice.get(idx).getID())
+                    .alpha(0.01f);
             googleMap.addMarker(markerOptions);
         }
         LatLng startLocation = new LatLng(36.369906, 127.345907);
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(startLocation));
         googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        drawLine(mGoogleMap);
         // map에 선 그리는 함수.
-        // googleMap.addPolyline(new PolylineOptions().add(latLngList[0],latLngList[1]).width(5).color(Color.GREEN));
+//        for (int i = 0; i < latLngList.length - 1; i++) {
+//            googleMap.addPolyline(new PolylineOptions().add(latLngList[i], latLngList[i + 1]).width(5).color(Color.GREEN));
+//        }
+        //googleMap.addPolyline(new PolylineOptions().add(latLngList[1], latLngList[6]).width(5).color(Color.GREEN));
+
 
     }
+    private List<MapCoordinate> makeCoordinate(){
+        coordinates = new ArrayList<>();
+        coordinates.add(new MapCoordinate(0, 1));
+        coordinates.add(new MapCoordinate(1, 2));
+        coordinates.add(new MapCoordinate(2, 3));
+        coordinates.add(new MapCoordinate(6, 7));
+        coordinates.add(new MapCoordinate(7, 8));
+        coordinates.add(new MapCoordinate(8, 9));
+        coordinates.add(new MapCoordinate(10, 11));
+        coordinates.add(new MapCoordinate(11, 12));
+        coordinates.add(new MapCoordinate(12, 13));
+        coordinates.add(new MapCoordinate(13, 14));
+
+
+        coordinates.add(new MapCoordinate(0, 6));
+        coordinates.add(new MapCoordinate(6, 10));
+        coordinates.add(new MapCoordinate(1, 4));
+        coordinates.add(new MapCoordinate(4, 5));
+        coordinates.add(new MapCoordinate(5, 8));
+        coordinates.add(new MapCoordinate(8, 13));
+        coordinates.add(new MapCoordinate(7, 11));
+        coordinates.add(new MapCoordinate(3, 9));
+        coordinates.add(new MapCoordinate(9, 14));
+        return coordinates;
+    }
+
+    private void drawLine(GoogleMap googleMap){
+        for(int i=0; i<coordinates.size(); i++){
+            googleMap.addPolyline(new PolylineOptions().add(latLngList[coordinates.get(i).getX_dot()], latLngList[coordinates.get(i).getY_dot()]).width(5).color(Color.GREEN));
+        }
+    }
+
     private void updateNode() {
         firebaseDatabaseRef.child("registedNode").addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 EndDevice a = dataSnapshot.getValue(EndDevice.class);
                 list_EndDevice.add(a);
-                if (a.getID().equals("충대후문")){
+                if (a.getID().equals("24")) {
                     onMapReady(mGoogleMap);
                     initNode(mGoogleMap);
                 }
