4de2c896fb92ffe9111b8af8fad47c937a8d005f
JeonSeongBae <tjdqo6042@naver.com>
2018-08-16 16:50:52 +0900

    Recovering Computer

--- testVersion/app/src/main/java/com/example/leechungwan/testversion/MainActivity.java
+++ testVersion/app/src/main/java/com/example/leechungwan/testversion/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.leechungwan.testversion;
 
 import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,6 +20,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -212,7 +214,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
             @Override
             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
+                
             }
 
             @Override
